30b918681bc703aca94956cd3218d747b805ef19
TR911 <TR-911@outlook.com>
2019-03-11 01:01:44 +0800

    爱心树

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+app.listen(80, () => {
+    console.log('server running...');
+})
+
+
+app.use('/public', express.static('./public'))
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'index.html'))
+
+})
+
+
+
+
+
+
